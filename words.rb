1f2ccd5b9bf02254568cd2e680d1e28d780bc5f9
Scott Baitz <scott.w.baitz@gmail.com>
2023-03-19 16:26:59 -0600

    Print a list of words longer than 3

---
+++ words.rb
@@ -0,0 +1,7 @@
+words = ["statuesque", "pig", "cycle", "abaft", "embarrass", "hilarious", "dog", "act", "amuck", "sour", "incredible", "didactic"]
+
+words.each do |word|
+  if word.length > 3
+    puts word
+  end
+end
